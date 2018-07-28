5e4ae4cd5553b498ce196efab8bfe698494c1a7f
jim29800 <jbouvier1173@gmail.com>
2018-07-28 15:05:41 +0200

    login

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function index(Request $request)
+    {
+        $json = $request->getContent();
+        // $vars = json_decode($json, true);
+        $jwtManager = $this->container
+            ->get('lexik_jwt_authentication.jwt_manager');
+        $user = new User();
+        return $this->json([
+            // 'vars' => $vars,
+            'token' => $jwtManager->create($user),
+        ]);
+    }
+}
